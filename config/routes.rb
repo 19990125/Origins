ad174eb390607e9d1361050228363a6501e5f2cc
19990125 <kyou.0125@i.softbank.jp>
2021-11-13 10:58:52 +0000

    更新３

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'homes/top' => 'homes#top'
   get 'submissions/search' => 'submissions#search'
