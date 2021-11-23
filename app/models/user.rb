381892092e437d194f5ab4614366827c9bfb04ed
19990125 <kyou.0125@i.softbank.jp>
2021-11-23 06:38:48 +0000

    更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :submissions, dependent: :destroy
-  attachment :icon_image
   has_many :favorites, dependent: :destroy
 
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
@@ -13,6 +12,7 @@ class User < ApplicationRecord
 
   has_many :relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
+  attachment :icon_image
 
   def follow(user_id)
     relationships.create(followed_id: user_id)
