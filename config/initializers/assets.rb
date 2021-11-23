632c49420bcd1d10c17afe569a6dc98267737a3f
19990125 <kyou.0125@i.softbank.jp>
2021-11-23 12:22:56 +0000

    更新

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[homes/top]
+#Rails.application.config.assets.precompile += %w[homes/top]
+
+Rails.application.config.assets.precompile += %w(users.css submissions.css relationships.css favorites.css homes.css *.js )
