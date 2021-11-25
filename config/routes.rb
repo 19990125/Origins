2923d03beedb2e0bc95eb606e1605f49ffc8fcf7
19990125 <kyou.0125@i.softbank.jp>
2021-11-25 11:17:31 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  # get 'profile/users/:id' => 'users#show', as: 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'homes/top' => 'homes#top'
   get 'submissions/search' => 'submissions#search'
-  get 'submissions/new' => 'submissions#new'
-  resources :submissions, only: %i[index show create destroy] do
+  # get 'submissions/new' => 'submissions#new'
+  resources :submissions, only: %i[index new show create destroy] do
     resources :favorites, only: %i[index create destroy]
   end
+
   get 'users/unsubscribe' => 'users#unsubscribe'
   resources :users, only: %i[show edit destroy update] do
     get 'submissions' => 'favorites#index'
